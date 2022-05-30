55b412b23724398bd62d486fa95265fe7abb2adf
luigi <luigilamastra@gmail.com>
2022-05-30 18:01:17 +0200

    deploy issues

--- pages/api/storeTweets.js
+++ pages/api/storeTweets.js
@@ -1,24 +1,24 @@
 
-import {exampleTweet} from '../../exampleTweet'
-import { getAll } from '../../requests/getAll';
-import { getMostDiscussedTweets } from '../../requests/getMostDiscussedTweets'
-import { getRatioedCollection, pushRatioed } from '../../requests/getRatioedCollection'
-async function handler(req, res) {
-    const tweets = await (await getAll()).results;
+// import {exampleTweet} from '../../exampleTweet'
+// import { getAll } from '../../requests/getAll';
+// import { getMostDiscussedTweets } from '../../requests/getMostDiscussedTweets'
+// import { getRatioedCollection, pushRatioed } from '../../requests/getRatioedCollection'
+// async function handler(req, res) {
+//     const tweets = await (await getAll()).results;
 
-const mostDiscussed = await getMostDiscussedTweets(tweets)
-const ratioed = mostDiscussed.filter(t => t.ratio > 1)
-await pushRatioed(ratioed)
-const result = await getRatioedCollection()
+// const mostDiscussed = await getMostDiscussedTweets(tweets)
+// const ratioed = mostDiscussed.filter(t => t.ratio > 1)
+// await pushRatioed(ratioed)
+// const result = await getRatioedCollection()
 
-    res.status(200).json(result)
-
-
-
-}
+//     res.status(200).json(result)
 
 
-export default handler
+
+// }
+
+
+// export default handler
 
 // getMostdiscussedTweets of the day
 
