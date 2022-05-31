07b3f9fb7b5b5246b0f1b8310b32ea5b23a1b340
luigi <luigilamastra@gmail.com>
2022-05-31 11:56:20 +0200

    deployment

--- next.config.js
+++ next.config.js
@@ -1,37 +1,9 @@
-/** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-const nextConfig = {
-  reactStrictMode: true,
-  reactStrictMode: true,
-  images: {
-    domains: ['pbs.twimg.com', 'example2.com'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-        config.resolve.fallback = {
-            fs: false
-        }
-    }
-
-    return config;
-}
-}
-
-module.exports =(phase, nextConfig) => {
-  return withBundleAnalyzer(nextConfig)
-}
-
-
-
-
-
-
-
-
-// module.exports = {
+// /** @type {import('next').NextConfig} */
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+// const nextConfig = {
+//   reactStrictMode: true,
 //   reactStrictMode: true,
 //   images: {
 //     domains: ['pbs.twimg.com', 'example2.com'],
@@ -47,3 +19,31 @@ module.exports =(phase, nextConfig) => {
 //     return config;
 // }
 // }
+
+// module.exports =(phase, nextConfig) => {
+//   return withBundleAnalyzer(nextConfig)
+// }
+
+
+
+
+
+
+
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['pbs.twimg.com', 'example2.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;
+}
+}
